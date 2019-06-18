70ee723ed144c3c1a4d70f77b961069c205036a3
Jorge Silva <jorge@fcn.edu.br>
2019-06-18 12:19:54 -0300

    Modulo Scraping terremoto
    Capta os ultimos 200 ocorrencias de terremotos

--- cliente/modulosDev/scrape.js
+++ cliente/modulosDev/scrape.js
@@ -6,38 +6,70 @@ const writeStream = fs.createWriteStream('terremotos.csv');
 request('http://monitorglobal.com.br/terremotos.html', (error, response, html) => {
   // --------------------------------------
   if (!error && response.statusCode == 200) {
-
     const $ = cheerio.load(html);
-
-    const siteHeading = $('#sismo_lista');
     
-    //console.log(siteHeading.text());
-    //console.log(siteHeading.find('div').text());
-    //console.log(siteHeading.children('div').text());
+    // criando uma objeto json
+    var ocorrencia = new Object();
+    var key  = 'Terremotos';
+    var head = 0;
+    ocorrencia[key] = [];  // Array vazio, com a chave "key"
 
-    //const output = siteHeading
-    //  .children('div')
-    //  .parent()
-    //  .text();  
-    //console.log(output);
+    $('#sismo_lista').each(function(){ 
+      /* [Ctrl+Shift+C]  (Copy/Copy Seletor)
+      ::: cabeçahlo 
+      #sismo_lista > div > div:nth-child(1) > div > strong        // Horário GMT/UTC
+      #sismo_lista > div > div:nth-child(2) > div > strong        // Horário Brasília
+      #sismo_lista > div > div:nth-child(3) > div > strong        // Intensidade
+      #sismo_lista > div > div:nth-child(4) > div > strong        // Magnitude
+      #sismo_lista > div > div:nth-child(5) > div > strong        // Profundidade
+      #sismo_lista > div > div:nth-child(6) > div > strong        // Local
+      #sismo_lista > div > div:nth-child(7) > div > strong        // Fonte
+      ::: dados conteudo
+      #sismo_lista > div > div:nth-child(1) > div                 // 18/06/2019 13:22
+      #sismo_lista > div > div:nth-child(2) > div                 // 18/06/2019 10:22 
+      #sismo_lista > div > div:nth-child(3) > div                 // Terremoto Forte
+      #sismo_lista > div > div:nth-child(4) > div                 // 6.4 
+      #sismo_lista > div > div:nth-child(5) > div                 // 16.08 km
+      #sismo_lista > div > div:nth-child(6) > div                 // 33km WSW of Tsuruoka, Japan 
+      #sismo_lista > div > div:nth-child(7) > div > a > img       // [+]info
+      #sismo_lista > div > div:nth-child(7) > div > a
+      */
 
-    //$('#sismo_lista').each((i, el) => {
-    //  const title = $(el)
-    //    .find('div')
-    //    .text()
-    //    .replace(/\s\s+/g, '');
+      if ( head == 0) {
+        var horario_GMTUTC    = $(this).find('#sismo_lista > div > div:nth-child(1) > div > strong ').text();
+        var horario_Brasilia  = $(this).find('#sismo_lista > div > div:nth-child(2) > div > strong ').text();
+        var intensidade       = $(this).find('#sismo_lista > div > div:nth-child(3) > div > strong ').text();
+        var magnitude         = $(this).find('#sismo_lista > div > div:nth-child(4) > div > strong ').text();
+        var profundidade      = $(this).find('#sismo_lista > div > div:nth-child(5) > div > strong ').text();
+        var local             = $(this).find('#sismo_lista > div > div:nth-child(6) > div > strong ').text();
+        var fonte             = $(this).find('#sismo_lista > div > div:nth-child(7) > div > strong ').text();
+        head = 1;
+      } else {
+        var horario_GMTUTC    = $(this).find('#sismo_lista > div > div:nth-child(1) > div ').text();
+        var horario_Brasilia  = $(this).find('#sismo_lista > div > div:nth-child(2) > div ').text();
+        var intensidade       = $(this).find('#sismo_lista > div > div:nth-child(3) > div ').text();
+        var magnitude         = $(this).find('#sismo_lista > div > div:nth-child(4) > div ').text();
+        var profundidade      = $(this).find('#sismo_lista > div > div:nth-child(5) > div ').text();
+        var local             = $(this).find('#sismo_lista > div > div:nth-child(6) > div ').text();
+        var fonte             = $(this).find('#sismo_lista > div > div:nth-child(7) > div  > a ').attr('href');
+      }
+      
+      var data = {
+        'horario_GMTUTC': horario_GMTUTC, 
+        'horario_Brasilia': horario_Brasilia,
+        'intensidade': intensidade,    
+        'magnitude': magnitude,      
+        'profundidade': profundidade, 
+        'local': local,    
+        'fonte': fonte    
+      };
 
-    //  console.log(title);
-    //});    
+      ocorrencia[key].push(data);
 
-    const output = siteHeading
-      .children('div')
-      .parent()
-      .text();  
+    });
 
-    console.log(output);
+    console.log(ocorrencia);
 
-    
   }
 });
 
